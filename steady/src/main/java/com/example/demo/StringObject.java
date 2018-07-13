a52c162def0663677bbb006e19e09ad3dead746d
jiangxinlin <jiangxinlin@xiaoantimes>
2018-07-13 14:32:53 +0800

    修改生产者消费者

--- steady/src/main/java/com/example/demo/StringObject.java
+++ steady/src/main/java/com/example/demo/StringObject.java
@@ -2,12 +2,31 @@ package com.example.demo;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
 
 /**
  * 生产者与消费者操作的公共变量
+ *
  * @author jiangxinlin
  * @version 2018-06-12
  */
 public class StringObject {
+
     public static List<String> list = new ArrayList<>();
+
+    public static void main(String[] args) throws InterruptedException {
+        // 默认是非公平锁，传入true则为公平锁
+        Lock lock = new ReentrantLock(true);
+        Condition condition = lock.newCondition();
+        for (int i = 0; i < 3; i++) {
+            ThreadProducer pThread = new ThreadProducer(lock, condition);
+            ThreadConsumer cThread = new ThreadConsumer(lock, condition);
+            pThread.start();
+            cThread.start();
+        }
+
+    }
+
 }
--- steady/src/main/java/com/example/demo/ThreadConsumer.java
+++ steady/src/main/java/com/example/demo/ThreadConsumer.java
@@ -1,15 +1,19 @@
 package com.example.demo;
 
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
 /**
  * 消费者线程
  * @author jiangxinlin
  * @version 2018-06-12
  */
 public class ThreadConsumer extends Thread {
-    private Consumer consumer;
+    private Consumer consumer = null;
 
-    public ThreadConsumer(Consumer consumer) {
-        this.consumer = consumer;
+    public ThreadConsumer(Lock lock, Condition condition) {
+        this.consumer = new Consumer(lock,condition);
     }
 
     @Override
@@ -23,6 +27,38 @@ public class ThreadConsumer extends Thread {
             }
             consumer.getValue();
         }
+    }
+
+    class Consumer {
+        private Lock lock;
+        private Condition condition;
+
+        public Consumer(Lock lock, Condition condition) {
+            super();
+            this.lock = lock;
+            this.condition = condition;
+        }
+
+        public void getValue() {
+            try {
+                lock.lock();
+                System.out.println(Thread.currentThread().getId() + "》》》进入消费者");
+                while (StringObject.list != null && StringObject.list.size() == 0) {
+                    //没值，不进行消费
+                    condition.await();
+                }
+                String value = StringObject.list.get(StringObject.list.size() - 1);
+                System.out.println(Thread.currentThread().getId() + "---消费的值是：" + value);
+                StringObject.list.remove(value);
+                condition.signalAll();
+
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            } finally {
+                lock.unlock();
+            }
+        }
+
     }
 
 }
